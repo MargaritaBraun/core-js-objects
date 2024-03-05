828e46dff6f73f04a92479f0c9f878126922f48b
Marharyta Parkalava <margobraun777@gmail.com>
2024-03-05 20:39:15 +0300

    feat: Rectangle and getJSON is done

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -199,8 +199,14 @@ function sellTickets(/* queue */) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  const r = {};
+  r.width = width;
+  r.height = height;
+  r.getArea = () => {
+    return width * height;
+  };
+  return r;
 }
 
 /**
@@ -213,8 +219,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 /**
